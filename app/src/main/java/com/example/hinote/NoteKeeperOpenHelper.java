f51fbba71e2ee458c19799c6859f3b109f150b9a
alazar-w <ej.dalvic@gmail.com>
2020-06-03 13:27:23 +0300

    table joining and using indexes to some columns done

--- app/src/main/java/com/example/hinote/NoteKeeperOpenHelper.java
+++ app/src/main/java/com/example/hinote/NoteKeeperOpenHelper.java
@@ -6,9 +6,12 @@ import android.database.sqlite.SQLiteOpenHelper;
 
 import androidx.annotation.Nullable;
 
+import com.example.hinote.NoteKeeperDatabaseContract.CourseInfoEntry;
+import com.example.hinote.NoteKeeperDatabaseContract.NoteInfoEntry;
+
 public class NoteKeeperOpenHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "NoteKeeper.db";
-    public static final int DATABASE_VERSION = 1;
+    public static final int DATABASE_VERSION = 2;
 
     //factory = is a mechanism we use to customize behavior of our database interaction,if don't really want to do that we put null and remove it from our constructors list
     //name = is the file name that contains our database
@@ -20,8 +23,12 @@ public class NoteKeeperOpenHelper extends SQLiteOpenHelper {
     //THE SQLiteDatabase class allows us to interact with the database. So that'll actually allow us to execute the SQL statements that create our tables
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(NoteKeeperDatabaseContract.CourseInfoEntry.SQL_CREATE_TABLE);
-        db.execSQL(NoteKeeperDatabaseContract.NoteInfoEntry.SQL_CREATE_TABLE);
+        db.execSQL(CourseInfoEntry.SQL_CREATE_TABLE);
+        db.execSQL(NoteInfoEntry.SQL_CREATE_TABLE);
+
+        //CREATING OUR INDEXES
+        db.execSQL(CourseInfoEntry.SQL_CREATE_INDEX1);
+        db.execSQL(NoteInfoEntry.SQL_CREATE_INDEX1);
 
         DatabaseDataWorker worker = new DatabaseDataWorker(db);
         worker.insertCourses();
@@ -29,8 +36,14 @@ public class NoteKeeperOpenHelper extends SQLiteOpenHelper {
     }
 
     //the purpose this method is to transition the database from an older version up to whatever version an app might currently need.
+    //old version is the database that's currently in and newVersion is the version we're updating in
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        //here i'm about to upgrade my database from version 1 to 2 after i added indexes in some of my columns
+        if (oldVersion < 2){
+            db.execSQL(CourseInfoEntry.SQL_CREATE_INDEX1);
+            db.execSQL(NoteInfoEntry.SQL_CREATE_INDEX1);
+        }
 
     }
 }
